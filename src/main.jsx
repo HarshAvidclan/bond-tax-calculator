0b3474d881df5ffa8c8458d79f3e6050a7200099
Harsh Patel <harshp.avidclan@gmail.com>
2026-01-07 17:58:03 +0530

    inex

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import BondCalculatorPage from './Components/BondCalculatorPage'
+import BondCalculatorPage from './Components/BondCalculatorPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
